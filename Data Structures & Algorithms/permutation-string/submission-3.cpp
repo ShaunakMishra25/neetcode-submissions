cfe7b73b0c2233bed18586c2e4fcaacd82cd26c1
Shaunak Mishra <125294993+ShaunakMishra25@users.noreply.github.com>
2026-09-04 19:18:45 +0530

    Add: permutation-string - submission-3

---
+++ Data Structures & Algorithms/permutation-string/submission-3.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
+
+        if(n > m) return false;
+
+        vector<int> s1_freq(26, 0);
+        vector<int> s2_freq(26, 0);
+
+        for(char& ch : s1) { // O(n)
+            s1_freq[ch-'a']++;
+        } 
+
+        int i = 0, j = 0;
+
+        //T.C O(m+n)
+        while(j < m){ // O (m)
+            s2_freq[s2[j] - 'a']++;
+
+            if(j - i + 1 > n) {
+                s2_freq[s2[i] - 'a']--;
+                i++;
+            }
+
+            if(s1_freq == s2_freq) {
+                return true;
+            }
+            j++;
+        }
+        return false;
+    }
+};
